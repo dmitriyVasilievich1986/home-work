f42889faa06d0427e64f65f13db85f91968587ca
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-06-04 21:32:49 +0300

    04.06.2020

--- interview preparation/preparation/Scripts/preparation/products/urls.py
+++ interview preparation/preparation/Scripts/preparation/products/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from .views import (
-    product_view,
-    new_product
-)
+# from django.urls import path
+from rest_framework import routers
+from .api import ProductViewSet
+# from .views import (
+#     product_view,
+#     new_product
+# )
 
-urlpatterns = [
-    path('', product_view, name="product"),
-    path('<int:id>', product_view, name="product"),
-    path('new/', new_product)
-]
+# urlpatterns = [
+#     path('', product_view, name="product"),
+#     path('<int:id>', product_view, name="product"),
+#     path('new/', new_product)
+# ]
+
+router = routers.DefaultRouter()
+router.register('api/products', ProductViewSet, 'Product')
+urlpatterns = router.urls
