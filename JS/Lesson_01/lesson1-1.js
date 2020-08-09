5891c5ce2f19444b3d6301055817cbfc2fd978fb
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-09 23:04:48 +0300

    lesson1-1 mk2

--- JS/Lesson_01/lesson1-1.js
+++ JS/Lesson_01/lesson1-1.js
@@ -1,3 +1,8 @@
-const tempCelcius = prompt('Enter temperature in celcius: ')
+// запрос значений от пользователя
+const tempCelcius = parseInt(prompt('Enter temperature in celcius: '))
+
+// перевод значения по формуле
 const tempFahrengeit = parseInt((9 / 5) * tempCelcius + 32)
+
+// вывод результата
 alert(`Temperature:\nCelcius: ${tempCelcius}\nFahrengeit: ${tempFahrengeit}`)
