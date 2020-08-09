a978cf7b3648108bdb857fa042d954bc981c0b83
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-09 23:01:44 +0300

    lesson1-1

---
+++ JS/Lesson_01/lesson1-1.js
@@ -0,0 +1,3 @@
+const tempCelcius = prompt('Enter temperature in celcius: ')
+const tempFahrengeit = parseInt((9 / 5) * tempCelcius + 32)
+alert(`Temperature:\nCelcius: ${tempCelcius}\nFahrengeit: ${tempFahrengeit}`)
