1739f9aa1dcf83dd405057ea7b28f0f1c291c383
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-17 20:31:00 +0300

    17.08.2020

--- JS/Lesson_03/lesson_3-5.js
+++
@@ -1,3 +0,0 @@
-"use strict"
-
-for (let x = 0; x < 10; console.log(x++));
--- JS/Lesson_03/lesson_3-6.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-// инициализируем цикл
-for (let x = 0; x < 10; x++) {
-    console.log('*'.repeat(x + 1));
-};
