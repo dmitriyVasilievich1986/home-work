e773b090f5884fb5c90fd8fc3dba4307eed9f96a
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-17 19:40:55 +0300

    задание 1

---
+++ JS/Lesson_03/lesson_3-1.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// инициализируем цикл
+for (let cycle = 0; cycle < 11; cycle++) {
+    if (cycle === 0) {
+        console.log("0-это ноль");  // выводим входной 0
+    } else {
+        const postScript = cycle % 2 === 0 ? '-четное число' : '-нечетное число'; // вычисляем остаток
+        console.log(`${cycle}${postScript}`);   // выводим получившееся выражение
+    }
+}
